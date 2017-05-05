7e2b8a071023f0d8bb1a4268ee0ce15ef002cb01
Wang Qi <491955882@qq.com>
2017-05-05 15:48:55 +0800

    手机PC修改

--- themes/default/js/menu.js
+++ themes/default/js/menu.js
@@ -184,8 +184,11 @@ $(document).ready(function(){
 	})	
 	/* 底部表单切换 */
 	$(document).on('click','.form-ren div',function(){
+		var wheight = $(window).height();
+		var bwheight =$('.bottom-form').height();
+		var x = parseInt(wheight-bwheight);
 		var bheight = $('.bottom-form').position().top;
-		if( bheight <= 524){
+		if( bheight <= x){
 			$('.bottom-form').animate({
 				"bottom":'-370px'
 			},800)
